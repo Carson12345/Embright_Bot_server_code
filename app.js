db590a3e851d82c729e39d6aedd10835a4d27a9e
embright00_2017 <graysonho@outlook.com>
2017-03-23 23:37:44 +0800

    can talk

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: "6aec7649-860b-45d4-af2f-35df460368df",
-    appPassword: "gugi20v9jRyJXdogq25sYuG"
+    appId: "0e4ad4e8-25e1-44b1-bfa6-6d0cd029a5f8",
+    appPassword: "w5igphWF1rDqCuNjBgNxHWK"
 });
 var bot = new builder.UniversalBot(connector);
 server.use(restify.bodyParser());
@@ -136,9 +136,45 @@ bot.dialog('/', new builder.IntentDialog({ recognizers: [recognizer] })
     .matches('get_definition', [
         function (session, args, next) 
             {
+            var address = session.message.address;
             var query = builder.EntityRecognizer.findEntity(args.entities, 'def_search');
             var queryUrl = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts&exintro&explaintext&exsentences=5&exlimit=max&gsrsearch=" + query.entity;
-            session.send(queryUrl, session.message.text);
+            request({
+                    url: queryUrl,
+                    method: "POST",
+                    json: true,   // <--Very important!!!
+                    
+                }, 
+                function(err, res, body) {
+                    //var myans = JSON.stringify(body);
+                    //console.log(body.query);
+                    //var body = JSON.stringify(body);
+                    var realpages = body.query.pages;
+                    var cards = new Array();
+                    var counter = 0;
+                    for (var i = 0 in realpages) 
+                    {
+                    if (counter > 4) { break; }
+                    cards.push(constructwikiCard(realpages[i]));
+                    console.log(realpages[i].title);
+                    counter = counter + 1;
+                    }
+                    // create reply with Carousel AttachmentLayout
+                    
+                    const reply = new builder.Message()
+                        .address(address)
+                        .text('Here are the results I found about the meaning of [' + query.entity+']')
+                        .attachmentLayout(builder.AttachmentLayout.carousel)
+                        .attachments(cards);
+                    bot.send(reply);
+                    //create cards
+                    
+
+                    
+                // `body` is a js object if request was successful
+                });
+                //end of request
+
             }
     ])
 
@@ -226,24 +262,36 @@ bot.dialog('/', new builder.IntentDialog({ recognizers: [recognizer] })
             next();
         },
         function (session, args, next) {
-            session.send('Here you are:) These are the topics related to ' + topic_to_learn_ID.entity, session.message.text);
+            session.send('Here you are:) These are the topics related to ' + topic_to_learn_ID.entity +' for you to start with.', session.message.text);
             // create reply with Carousel AttachmentLayout
             var cards = getCardsAttachments();
             var reply = new builder.Message(session)
             .attachmentLayout(builder.AttachmentLayout.carousel)
             .attachments(cards);
             session.send(reply);
-            builder.Prompts.text(session, 'Please choose one to start with');
+            builder.Prompts.text(session, 'please choose the one you are most interested in!');
         },
-        function (session, results) {
+        function (session, results,next) {
+            var topic_to_learn = results.response;
+            topic_to_learn_ID = topic_to_learn;
+            session.send('Nice choice! I am formulating some learning plans for you!', session.message.text);
+            var cards = getCardsAttachments();
+            var reply = new builder.Message(session)
+            .attachmentLayout(builder.AttachmentLayout.carousel)
+            .attachments(cards);
+            session.send(reply);
+            builder.Prompts.text(session, 'Try one of these learning plan!');
+        },
+         function (session, results,next) {
             var plan_chosen = results.response;
             plan_chosen_ID = plan_chosen;
-            session.send('Got it! The learning plan has been added to your profile', session.message.text);
-        }
+            session.send('Got it! The learning plan has been added to your profile! Start with the materials and opportunities in it, I will keep track of your progress and guide you!:)', session.message.text);
+         }
         
     ])
 
     .onDefault((session) => {
+        
         session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
     }));
 
@@ -264,7 +312,33 @@ if (process.env.IS_SPELL_CORRECTION_ENABLED === 'true') {
     });
 }
 
-//options
+//wikians
+function constructwikiCard(page) {
+                        return new builder.HeroCard()
+                            .title(page.title)
+                            .subtitle(page.extract.substr(0, 600).trim()+'...')
+                            .buttons([
+                                new builder.CardAction()
+                                    .title('More about this definition')
+                                    .type('openUrl')
+                                    .value("https://en.wikipedia.org/?curid=" + page.pageid)
+                            ]);
+                    }
+
+
+//topiccard
+// function topic_card() {
+//     return new builder.HeroCard()
+//         .title(hotel.name)
+//         .subtitle('%d stars. %d reviews. From $%d per night.', hotel.rating, hotel.numberOfReviews, hotel.priceStarting)
+//         .images([new builder.CardImage().url(hotel.image)])
+//         .buttons([
+//             new builder.CardAction()
+//                 .title('More details')
+//                 .type('openUrl')
+//                 .value('https://www.bing.com/search?q=hotels+in+' + encodeURIComponent(hotel.location))
+//         ]);
+// }
 
 //cards
 function getCardsAttachments(session) {
