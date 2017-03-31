943d9b5660d6b53cfd1fc6e35fc062e094f95457
embright00_2017 <graysonho@outlook.com>
2017-03-31 23:09:04 +0800

    ok

--- app.js
+++ app.js
@@ -474,7 +474,8 @@ function constructwikiCard(page) {
 function topic_card(planobj,key) {
     return new builder.HeroCard()
         .title(planobj[key]['PlanTitle'])
-        .subtitle(planobj[key]['PlanDetails'])
+        //.subtitle(planobj[key]['PlanDetails'])
+        .subtitle('This is a taiwan individual documentary project about a daugther and her lesbian mother')
         .images([
             //builder.CardImage.create(null, ('http://embrightweb.azurewebsites.net' + planobj[key]['PlanPicture']))
             builder.CardImage.create(null, ('https://priestesswalksalone.files.wordpress.com/2014/05/e9a696e9a081e59c9612.jpg?w=1024&h=426&crop=1'))
